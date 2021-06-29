8b7f5c9373fc7c5fa86f1fd2eee645936e092c02
Susan Tan <susan@getwellio.com>
2021-06-29 00:24:38 -0500

    Demo almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Checkbox, Dropdown, Form, Label, Radio } from 'semantic-ui-react'
+import { Input } from 'semantic-ui-react'
+import { useState } from 'react';
+import { conditions } from './conditions';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DemographicForm />
+        <ConditionsForm />
+        <MedicalQuestions />
       </header>
     </div>
   );
 }
 
+function DemographicForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [gender, setGender] = useState('');
+  const [dob, setDOB] = useState('');
+  const [phone, setPhone] = useState('');
+  const [maritalStatus, setMaritalStatus] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  }
+
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+  }
+
+  const handleDOBChange = (event) => {
+    setDOB(event.target.value);
+  }
+
+  const handlePhoneChange = (event) => {
+    setPhone(event.target.value);
+  }
+
+  const handleMaritalStatusChange = (maritalStatus) => {
+    setMaritalStatus(maritalStatus);
+  }
+
+  const maritalStatuses = [{val: "married"}, {val: "divorced"}, {val: "single"}, {val: "life partner"}, {val: "separated"}, {val: "widowed"}, {val: "other"}];
+  return (
+    <Container>
+      <h1>Demographic Data</h1>
+      <Label>Name <Input focus value={name} onChange={handleNameChange} /></Label>
+      <Label>Email <Input focus value={email} onChange={handleEmailChange} /></Label>
+      <Label>Address <Input focus value={address} onChange={handleAddressChange} /></Label>
+      <Label>Gender <Input focus value={gender} onChange={handleGenderChange} /></Label>
+      <Label>DOB <Input focus value={dob} onChange={handleDOBChange} /></Label>
+      <Label>Phone Number <Input focus value={phone} onChange={handlePhoneChange} /></Label>
+      <Form>
+          {maritalStatuses.map((maritalStatus, index) =>
+            <Form.field>
+            <Radio key={index} onClick={() => handleMaritalStatusChange(maritalStatus.val)} label={maritalStatus.val} />
+            </Form.field>
+          )}
+      </Form>
+    </Container>
+  )
+}
+
+function ConditionsForm() {
+  const [searchVal, setSearchVal] = useState('');
+  const [allConditions, setAllConditions] = useState(conditions);
+  const [filterBy, setFilterBy] = useState('Type');
+
+  const handleSearchValChange = (event) => {
+    setSearchVal(event.target.value);
+    let filteredConditions;
+    if (filterBy === "Condition") {
+      filteredConditions = conditions.filter(condition => condition.condition.toLowerCase().includes(searchVal.toLowerCase()));
+    } else {
+      filteredConditions = conditions.filter(condition => condition.type.toLowerCase().includes(searchVal.toLowerCase()));
+    }
+    setAllConditions(filteredConditions);
+    console.log(allConditions);
+  }
+
+  const onClickCheckbox = (index) => {
+    allConditions[index].isChecked = true;
+    setAllConditions(allConditions);
+  }
+
+  const options = [{ text: "Condition", key: "Condition", value: "Condition" }, { text: "Type", key: "Type", value: "Type" }];
+
+  const handleDropDown = (event) => {
+    setFilterBy(event.target.textContent);
+  }
+
+  return (
+    <Container>
+      <h1>Select Your Medical Conditions</h1>
+      <Label>Search by <Dropdown fluid selection options={options} onChange={handleDropDown} /><Input focus value={searchVal} onChange={handleSearchValChange} /></Label>
+      {allConditions.map((condition, index) =>
+        <Container key={index}>
+          <Checkbox onClick={() => onClickCheckbox(index)} label={condition.condition} />
+        </Container>
+      )}
+    </Container>
+  )
+}
+
+function MedicalQuestions() {
+  return (
+    <Container>
+      <h1>Medical Questions</h1>
+      <Label>Do you smoke any tobacco products?</Label>
+      <h2>If yes, how much and how often? </h2>
+
+    </Container>
+  )
+}
 export default App;
---
+++ src/conditions.js
@@ -0,0 +1,57 @@
+export const conditions = [{
+  "type": "cardiovascular",
+  "condition": "High blood pressure",
+  "isChecked": false,
+}, {
+  "type": "cardiovascular",
+  "condition": "Cardiac Arrest",
+  "isChecked": false,
+}, {
+  "type": "cardiovascular",
+  "condition": "Arrhythmia",
+  "isChecked": false,
+}, {
+  "type": "cardiovascular",
+  "condition": "Coronary heart disease",
+  "isChecked": false,
+}, {
+  "type": "cardiovascular",
+  "condition": "Coronary heart disease",
+  "isChecked": false,
+}, {
+  "type": "gastrointestinal",
+  "condition": "IBS",
+  "isChecked": false,
+}, {
+  "type": "gastrointestinal",
+  "condition": "Crohn's Disease",
+  "isChecked": false,
+}, {
+  "type": "gastrointestinal",
+  "condition": "Gallstones",
+  "isChecked": false,
+}, {
+  "type": "psychological",
+  "condition": "Depression",
+  "isChecked": false,
+}, {
+  "type": "psychological",
+  "condition": "Anxiety",
+  "isChecked": false,
+}, {
+  "type": "psychological",
+  "condition": "Stress",
+  "isChecked": false,
+}, {
+  "type": "psychological",
+  "condition": "Insomnia",
+  "isChecked": false,
+}, {
+  "type": "other",
+  "condition": "Cancer",
+  "isChecked": false,
+}, {
+  "type": "other",
+  "condition": "Diabetes",
+  "isChecked": false,
+}]
