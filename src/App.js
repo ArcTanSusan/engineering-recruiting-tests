09c970756c3e2b85c9ed07681e7d76d62bae328c
Susan Tan <susan@getwellio.com>
2021-06-29 01:34:32 -0500

    Add submit buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Checkbox, Dropdown, Form, Label, Radio } from 'semantic-ui-react'
+import { Button, Container, Checkbox, Dropdown, Form, Label, Radio } from 'semantic-ui-react'
 import { Input } from 'semantic-ui-react'
 import { useState } from 'react';
 import { conditions } from './conditions';
 
 function App() {
+  const [demographicInfo, setSubmittedDemographicInfo] = useState({});
+  const [conditionsInfo, setSubmittedConditionsInfo] = useState({})
+  const [medicalInfo, setSubmittedMedicalInfo] = useState({})
+
+  const submitDemographicForm = (info) => {
+    setSubmittedDemographicInfo(info);
+    console.log("demo:", info);
+  }
+
+  const submitConditionsInfo = (info) => {
+    setSubmittedConditionsInfo(info);
+    console.log("conditions:", info);
+  }
+
+  const submitMedicalInfo = (info) => {
+    setSubmittedMedicalInfo(info);
+    console.log("medical:", info);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <DemographicForm />
-        <ConditionsForm />
-        <MedicalQuestions />
+        <DemographicForm submit={submitDemographicForm} />
+        <ConditionsForm submit={submitConditionsInfo} />
+        <MedicalQuestions submit={submitMedicalInfo} />
+        <Summary demographicInfo={demographicInfo} conditionsInfo={conditionsInfo} medicalInfo={medicalInfo} />
+        <Terms />
       </header>
     </div>
   );
 }
 
-function DemographicForm() {
+function DemographicForm(props) {
+  const {submit} = props;
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [address, setAddress] = useState('');
@@ -54,6 +75,19 @@ function DemographicForm() {
     setMaritalStatus(maritalStatus);
   }
 
+  const onSubmit = () => {
+    const info = {
+      "name": name,
+      "email": email,
+      "address": address,
+      "gender": gender,
+      "dob": dob,
+      "phone": phone,
+      "maritalStatus": maritalStatus,
+    }
+    submit(info);
+  }
+
   const maritalStatuses = [{val: "married"}, {val: "divorced"}, {val: "single"}, {val: "life partner"}, {val: "separated"}, {val: "widowed"}, {val: "other"}];
   return (
     <Container>
@@ -64,18 +98,18 @@ function DemographicForm() {
       <Label>Gender <Input focus value={gender} onChange={handleGenderChange} /></Label>
       <Label>DOB <Input focus value={dob} onChange={handleDOBChange} /></Label>
       <Label>Phone Number <Input focus value={phone} onChange={handlePhoneChange} /></Label>
-      <Form>
-          {maritalStatuses.map((maritalStatus, index) =>
-            <Form.field>
-            <Radio key={index} onClick={() => handleMaritalStatusChange(maritalStatus.val)} label={maritalStatus.val} />
-            </Form.field>
-          )}
-      </Form>
+      <Label>Marital Status
+        {maritalStatuses.map((maritalStatus, index) =>
+          <Radio name="group" key={index} onClick={() => handleMaritalStatusChange(maritalStatus.val)} label={maritalStatus.val} />
+        )}
+      </Label>
+      <Button content="Submit" primary onClick={onSubmit} />
     </Container>
   )
 }
 
-function ConditionsForm() {
+function ConditionsForm(props) {
+  const {submit} = props;
   const [searchVal, setSearchVal] = useState('');
   const [allConditions, setAllConditions] = useState(conditions);
   const [filterBy, setFilterBy] = useState('Type');
@@ -103,6 +137,13 @@ function ConditionsForm() {
     setFilterBy(event.target.textContent);
   }
 
+  const onSubmit = () => {
+    const info = {
+      "conditions": conditions,
+    }
+    submit(info);
+  }
+
   return (
     <Container>
       <h1>Select Your Medical Conditions</h1>
@@ -112,17 +153,92 @@ function ConditionsForm() {
           <Checkbox onClick={() => onClickCheckbox(index)} label={condition.condition} />
         </Container>
       )}
+    <Button content="Submit" primary onClick={onSubmit} />
     </Container>
   )
 }
 
-function MedicalQuestions() {
+function MedicalQuestions(props) {
+  const {submit} = props;
+  const [isSmoker, setIsSmoker] = useState(false);
+  const [smokeFreq, setSmokeFreq] = useState('');
+  const [isAlcoholUser, setIsAlchoholUser] = useState(false);
+  const [drinkFreq, setDrinkFreq] = useState('');
+  const [isDrugUser, setIsDrugUser] = useState(false);
+  const [drugFreq, setDrugFreq] = useState('');
+
+  const handleSmokeFreq = (event) => {
+    setSmokeFreq(event.target.value);
+  }
+
+  const handleDrinkFreq = (event) => {
+    setDrinkFreq(event.target.value);
+  }
+
+  const handleDrugFreq = (event) => {
+    setDrugFreq(event.target.value);
+  }
+
+  const onSubmit = () => {
+    const info = {
+      "isSmoker": isSmoker,
+      "smokeFreq": smokeFreq,
+      "isAlcoholUser": isAlcoholUser,
+      "drinkFreq": drinkFreq,
+      "isDrugUser": isDrugUser,
+      "drugFreq": drugFreq,
+    }
+    submit(info);
+  }
+
+
   return (
     <Container>
       <h1>Medical Questions</h1>
-      <Label>Do you smoke any tobacco products?</Label>
-      <h2>If yes, how much and how often? </h2>
+      <Label>Do you smoke any tobacco products?
+        <Radio name="group" label="Yes" onClick={() => setIsSmoker(true)} />
+        <Radio name="group" label="No" onClick={() => setIsSmoker(false)} />
+      </Label>
+      <Label>If yes, how much and how often?<Input val={smokeFreq} onChange={handleSmokeFreq} /></Label>
+      <Label>Do you drink alcohol?
+        <Radio name="group" label="Yes" onClick={() => setIsAlchoholUser(true)} />
+        <Radio name="group" label="No" onClick={() => setIsAlchoholUser(false)} />
+      </Label>
+      <Label>If yes, how much and how often?<Input val={drinkFreq} onChange={handleDrinkFreq} /></Label>
+      <Label>Have you regularly used illicit drugs?
+        <Radio name="group" label="Yes" onClick={() => setIsDrugUser(true)}/>
+        <Radio name="group" label="No" onClick={() => setIsDrugUser(false)}/>
+      </Label>
+      <Label>If yes, how much and how often?<Input val={drugFreq} onChange={handleDrugFreq} /></Label>
+    
 
+    </Container>
+  )
+}
+
+function Summary(props) {
+  const {demographicInfo, conditionsInfo, medicalInfo} = props;
+  return (
+    <Container>
+      <h1>Summary of Your Medical Profile</h1>
+      <h2>Demographic Info</h2>{Object.entries(demographicInfo).map(obj => {
+      return (<div>{obj}</div>)})}
+      <h2>Conditions Info</h2><p>{conditionsInfo}</p>
+      <h2>Medical Short Answers</h2><p>{medicalInfo}</p>
+    </Container>
+  )
+}
+
+function Terms(props) {
+  const {submit} = props;
+  const onSubmit = () => {
+    submit();
+  }
+  return (
+    <Container>
+      <p>Nullam quis risus eget urna mollis ornare vel eu leo. Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna.
+Etiam porta sem malesuada magna mollis euismod. Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis euismod semper. Donec ullamcorper nulla non metus auctor fringilla.</p>
+    <Button content="Submit" primary onClick={onSubmit} />
     </Container>
   )
 }
